577de40d033a510427f307e413b7312a4d2dcefb
Sky Wan <contactskywan@gmail.com>
2019-12-01 23:15:49 -0500

    please

--- server/controllers/Pawpost.js
+++ server/controllers/Pawpost.js
@@ -117,15 +117,20 @@ const profilePage = (req, res) => {
 };
 
 const feedPage = (req, res) => {
-  Pawpost.PawpostModel.findByOwner(req.session.account._id, (err, docs) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).json({ error: "An error occurred" });
+  return Pawpost.PawpostModel.findByOwner(
+    req.session.account._id,
+    (err, docs) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json({ error: "An error occurred" });
+      } else {
+        return res.render("app", {
+          csrfToken: req.csrfToken(),
+          pawposts: docs
+        });
+      }
     }
-
-    return res.render("app", { csrfToken: req.csrfToken(), pawposts: docs });
-  });
-  return;
+  );
 };
 
 const getAllUsersPawposts = (req, res) => {
