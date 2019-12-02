61e4536e8692d8025d6bd7b0873ed1c3427ed828
Sky Wan <contactskywan@gmail.com>
2019-12-01 23:43:02 -0500

    please work

--- server/controllers/Pawpost.js
+++ server/controllers/Pawpost.js
@@ -124,10 +124,11 @@ const feedPage = (req, res) => {
         console.log(err);
         return res.status(400).json({ error: "An error occurred" });
       } else {
-        return res.render("app", {
+        res.render("app", {
           csrfToken: req.csrfToken(),
           pawposts: docs
         });
+        return res.status(200).json({ message: "Success" });
       }
     }
   );
