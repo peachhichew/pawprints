a5d63bb7a4af25f621ccba8495f9a219dc9f9808
Sky Wan <contactskywan@gmail.com>
2019-12-01 23:35:58 -0500

    try to fix circleCI error

--- server/controllers/Pawpost.js
+++ server/controllers/Pawpost.js
@@ -123,8 +123,7 @@ const feedPage = (req, res) => {
       return res.status(400).json({ error: "An error occurred" });
     } else {
       res.render("app", { csrfToken: req.csrfToken(), pawposts: docs });
-
-      return res.status(200).json({ message: "Page loaded successfully" });
+      return null;
     }
   });
 };
